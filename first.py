33b9f86f56220461deb5b274041ac5748513127a
Maresh Nani <mareshgunji@gmail.com>
2021-06-27 11:27:40 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,46 @@
+#print("hello world")
+#name='maresh'
+#print(name)
+#name=input("enter name:")
+#print(type(name))
+#a=1
+#print(float(a))
+#print(bool(a))
+#print(str(a))
+#print("AIRTHEMATIC OPERATION")
+# a=10
+#b=2
+#print("ADDITION = " , a+b)
+#print("SUBTRACTION =" , a-b)
+#print("DIVISON =" , a/b)
+#print("MODULUS REMAINDER =" , a%b)
+#print("MULTIPICATION =" , a*b)
+#print("EXPONENT =" , a**b)
+
+nam="maresh"
+print(nam)
+n='nani'
+print(n)
+print(nam[0])
+print(nam[-1])
+print(nam[0:2])
+print(nam[::-1])
+#nam[0]="n" string data is immutable
+print(len(nam))
+name="maresh" + " " +"nani"
+print(name)
+print(name *2)
+print(nam.upper())
+print(nam.lower())
+chu="  my name is nani  "
+print(chu.strip())
+chi="my name is ali   "
+print(chi.rstrip())
+chm="   my name"
+print(chm.lstrip())
+#count is used to find the itterative letters
+m=" do hardwork extremly"
+print(m.count('e'))
+print(m.count('a',6))
+age=5
+print("my age is {0} " .format(age))
